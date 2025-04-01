dd86d5578aa3137da06b1edeb73fd6a481041f26
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-04-01 08:50:11 -0500

    feat: update 8.5 assingment

--- Python Data Structures/assingments.py
+++ Python Data Structures/assingments.py
@@ -68,4 +68,59 @@ if count > 0:
 else:
     print("No lines found with X-DSPAM-Confidence:")
 
+"""
+8.4 Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in python sort() order as shown in the desired output.
+You can download the sample data at http://www.py4e.com/code3/romeo.txt
+"""
+# Prompt for file name
+fname = input("Enter file name: ")
+
+try:
+    fhand = open(fname)
+except:
+    print("File cannot be opened:", fname)
+    quit()
+
+words_list = []  # List to store unique words
+
+# Read file line by line
+for line in fhand:
+    words = line.split()  # Split line into words
+    for word in words:
+        if word not in words_list:
+            words_list.append(word)  # Add only unique words
+
+words_list.sort()  # Sort list in alphabetical order
+print(words_list)
+
+""""
+8.5 Open the file mbox-short.txt and read it line by line. When you find a line that starts with 'From ' like the following line:
+From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+You will parse the From line using split() and print out the second word in the line (i.e. the entire address of the person who sent the message). Then print out a count at the end.
+Hint: make sure not to include the lines that start with 'From:'. Also look at the last line of the sample output to see how to print the count.
+
+You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+"""
+# Prompt for file name
+fname = input("Enter file name: ")
+
+try:
+    fhand = open(fname)
+except:
+    print("File cannot be opened:", fname)
+    quit()
+
+count = 0  # Initialize count
+
+# Read file line by line
+for line in fhand:
+    if line.startswith('From '):  # Check if line starts with 'From ' (not 'From:')
+        words = line.split()  # Split the line into words
+        print(words[1])  # Print the email address (second word)
+        count += 1  # Increment count
+
+# Print total count
+print("There were", count, "lines in the file with From as the first word")
+
+
 
