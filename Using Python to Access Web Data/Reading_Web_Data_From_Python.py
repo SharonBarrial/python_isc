9c801c06c3c567ad10c79d2775252b236b473c24
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-04-03 10:05:16 -0500

    feat: Create Reading_Web_Data_From_Python_Quiz.py

---
+++ Using Python to Access Web Data/Reading_Web_Data_From_Python.py
@@ -0,0 +1,40 @@
+"""
+Question 1
+Answer: file handle
+
+Question 2
+Answer: mysock.recv()
+
+Question 3
+Answer: href="(.+)"
+
+Question 4
+Answer: mysock.connect()
+
+Question 5
+Answer: Content-Type:
+
+Question 6
+Answer: That the web site allows scraping
+
+Question 7
+Answer: It repairs and parses HTML to make it easier for a program to understand
+
+Question 8
+Answer: A list of all the anchor tags (<a..) in the HTML from the URL
+
+Question 9
+Answer: UTF-8
+
+Question 10
+Answer: **
+
+Question 11
+Answer: line
+
+Question 12
+Answer: Unicode
+
+Question 13
+Answer: decode()
+"""
