f810943564f22df0657c91429cc8275be973141b
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-04-06 10:23:33 -0500

    Create eXtensibleMarkupLanguage.py

---
+++ Using Python to Access Web Data/eXtensibleMarkupLanguage.py
@@ -0,0 +1,37 @@
+"""
+✅ 1. What is the name of the Python 2.x library to parse XML data?
+✔️ xml.etree.ElementTree
+
+✅ 2. Which of the following are not commonly used serialization formats?
+✔️ HTTP
+✔️ TCP
+✔️ Dictionaries
+(Estas no son formatos de serialización como JSON o XML)
+
+✅ 3. In this XML, which are the "simple elements"?
+✔️ name
+✔️ phone
+(Tienen solo texto y no contienen otros elementos)
+
+✅ 4. In the following XML, which are attributes?
+✔️ type
+✔️ hide
+
+✅ 5. Which node is the parent node of node <e>?
+✔️ c
+
+✅ 6. What text value would we find at path /a/c/e?
+✔️ Z
+
+✅ 7. What is the purpose of XML Schema?
+✔️ To establish a contract as to what is valid XML
+
+✅ 8. If you want to limit values to a specific list, which tag would you use?
+✔️ xs:enumeration
+
+✅ 9. What is a good time zone to use when exchanging data over APIs?
+✔️ Universal Time / GMT
+
+✅ 10. Which of the following dates is in ISO8601 format?
+✔️ 2002-05-30T09:30:10Z
+"""
