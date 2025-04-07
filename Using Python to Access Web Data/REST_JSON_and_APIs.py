11d131c8b2023082a045d8561ed7990bcbcbfd90
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-04-06 21:36:21 -0500

    Create REST_JSON_and_APIs.py

---
+++ Using Python to Access Web Data/REST_JSON_and_APIs.py
@@ -0,0 +1,33 @@
+"""
+1. Who is credited with getting the JSON movement started?
+✅ Douglas Crockford
+
+2. Which of the following is true about an API?
+✅ An API is a contract that defines how to use a software library
+
+3. What is the method used to parse a string containing JSON data so that you can work with the data in Python?
+✅ json.loads()
+
+4. What kind of variable will you get in Python when the following JSON is parsed: [ "Glenn", "Sally", "Jen" ]
+✅ A list with three items
+
+5. Which of the following is not true about the service-oriented approach?
+✅ An application runs together all in one place
+
+6. Which of these two web service approaches is preferred in most modern service-oriented applications?
+✅ REST - Representational state transfer
+
+7. What library call do you make to append properly encoded parameters to the end of a URL?
+✅ urllib.parse.urlencode()
+
+8. What happens when you exceed the Google geocoding API rate limit?
+✅ You cannot use the API for 24 hours
+(Este es el comportamiento más común para usuarios no autenticados o con claves gratuitas.)
+
+9. What protocol does Twitter use to protect its API?
+✅ OAuth
+
+10. What header does Twitter use to tell you how many more API requests you can make before you will be rate limited?
+✅ x-rate-limit-remaining
+
+"""
