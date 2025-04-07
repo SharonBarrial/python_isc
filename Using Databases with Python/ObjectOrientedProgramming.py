33bfbfa4768a3cf5498f7c9d8f550b5f75b90b3e
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-04-06 22:02:42 -0500

    Create ObjectOrientedProgramming.py

---
+++ Using Databases with Python/ObjectOrientedProgramming.py
@@ -0,0 +1,45 @@
+"""
+Which came first, the instance or the class?
+
+Answer: class
+
+In Object Oriented Programming, what is another name for the "attributes" of an object?
+
+Answer: fields
+
+At the moment of creation of a new object, Python looks at the _________ definition to define the structure and capabilities of the newly created object.
+
+Answer: class
+
+Which of the following is NOT a good synonym for "class" in Python?
+
+Answer: direction
+
+What does this Python statement do if PartyAnimal is a class?
+
+Answer: Use the PartyAnimal template to make a new object and assign it to zap
+
+What is the syntax to look up the fullname attribute in an object stored in the variable colleen?
+
+Answer: colleen.fullname
+
+Which of these statements is used to indicate that class A will inherit all the features of class B?
+
+Answer: class A(B) :
+
+What keyword is used to indicate the start of a method in a Python class?
+
+Answer: def
+
+What is "self" typically used for in a Python method within a class?
+
+Answer: To refer to the instance in which the method is being called
+
+What does the Python dir() function show when we pass an object into it as a parameter?
+
+Answer: It shows the methods and attributes of the object
+
+Which of the following is rarely used in Object Oriented Programming?
+
+Answer: Destructor
+"""
