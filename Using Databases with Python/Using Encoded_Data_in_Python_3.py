e1477c0501cb8e0bb4f8dbeea2a3a358ba943d5f
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-04-06 21:56:29 -0500

    Create Using Encoded_Data_in_Python_3.py

---
+++ Using Databases with Python/Using Encoded_Data_in_Python_3.py
@@ -0,0 +1,21 @@
+"""
+What is the most common Unicode encoding when moving data between systems?
+
+Answer: UTF-8
+
+What is the ASCII character that is associated with the decimal value 42?
+
+Answer: *
+
+What word does the following sequence of numbers represent in ASCII: 108, 105, 115, 116
+
+Answer: list
+
+How are strings stored internally in Python 3?
+
+Answer: Unicode
+
+When reading data across the network (i.e., from a URL) in Python 3, what method must be used to convert it to the internal format used by strings?
+
+Answer: decode()
+"""
